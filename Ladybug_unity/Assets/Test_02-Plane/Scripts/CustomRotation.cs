b8ed2cd769fbf574b77d23f63a6801c51a536ade
ben <mpsoft_2005@126.com>
2019-06-18 23:51:12 +0800

    update ladybug unity

--- Ladybug_unity/Assets/Test_02-Plane/Scripts/CustomRotation.cs
+++ Ladybug_unity/Assets/Test_02-Plane/Scripts/CustomRotation.cs
@@ -13,6 +13,46 @@ public class CustomRotation : MonoBehaviour
         Debug.LogFormat("({0}, {1}, {2})", v.x, v.y, v.z);
     }
 
+    void DebugMesh(Mesh mesh)
+    {
+        Debug.Log("mesh.vertices.Length=" + mesh.vertices.Length);
+        Debug.Log("mesh.normals.Length=" + mesh.normals.Length);
+        Debug.Log("mesh.triangles.Length=" + mesh.triangles.Length);
+        Debug.Log("mesh.uv.Length=" + mesh.uv.Length);
+
+        for (int i = 0; i < mesh.vertices.Length; i++)
+        {
+            Vector3 v = mesh.vertices[i];
+            Debug.LogFormat("v {0} {1} {2}", v.x, v.y, v.z);
+        }
+
+        for (int i = 0; i < mesh.uv.Length; i++)
+        {
+            Vector2 vt = mesh.uv[i];
+            Debug.LogFormat("vt {0} {1}", vt.x, vt.y);
+        }
+
+        for (int i = 0; i < mesh.normals.Length; i++)
+        {
+            Vector3 vn = mesh.normals[i];
+            Debug.LogFormat("vn {0} {1} {2}", vn.x, vn.y, vn.z);
+        }
+
+        for (int i = 0; i < mesh.triangles.Length / 3; i++)
+        {
+            int i0 = mesh.triangles[i * 3];
+            int i1 = mesh.triangles[i * 3 + 1];
+            int i2 = mesh.triangles[i * 3 + 2];
+
+            Debug.LogFormat("tri {0} {1} {2}", i0, i1, i2);
+
+            Debug.LogFormat("tri ({0} {1} {2})/({3} {4})/({5} {6} {7})  ({8} {9} {10})/({11} {12})/({13} {14} {15})  ({16} {17} {18})/({19} {20})/({21} {22} {23})",
+                mesh.vertices[i0].x, mesh.vertices[i0].y, mesh.vertices[i0].z, mesh.uv[i0].x, mesh.uv[i0].y, mesh.normals[i0].x, mesh.normals[i0].y, mesh.normals[i0].z,
+                mesh.vertices[i1].x, mesh.vertices[i1].y, mesh.vertices[i1].z, mesh.uv[i1].x, mesh.uv[i1].y, mesh.normals[i1].x, mesh.normals[i1].y, mesh.normals[i1].z,
+                mesh.vertices[i2].x, mesh.vertices[i2].y, mesh.vertices[i2].z, mesh.uv[i2].x, mesh.uv[i2].y, mesh.normals[i2].x, mesh.normals[i2].y, mesh.normals[i2].z);
+        }
+    }
+
     // Use this for initialization
     void Start()
     {
@@ -41,37 +81,7 @@ public class CustomRotation : MonoBehaviour
         Debug.Log("Light dir(world space):");
         PrintVector3(light.transform.forward);
 
-        Mesh mesh = meshFilter.mesh;
-        Debug.Log("mesh.vertices.Length=" + mesh.vertices.Length);
-        Debug.Log("mesh.normals.Length=" + mesh.normals.Length);
-        Debug.Log("mesh.triangles.Length=" + mesh.triangles.Length);
-        Debug.Log("mesh.uv.Length=" + mesh.uv.Length);
-
-        for (int i = 0; i < mesh.vertices.Length; i++)
-        {
-            Vector3 v = mesh.vertices[i];
-            Debug.LogFormat("v {0} {1} {2}", v.x, v.y, v.z);
-        }
-
-        for (int i = 0; i < mesh.uv.Length; i++)
-        {
-            Vector2 vt = mesh.uv[i];
-            Debug.LogFormat("vt {0} {1}", vt.x, vt.y);
-        }
-
-        for (int i = 0; i < mesh.normals.Length; i++)
-        {
-            Vector3 vn = mesh.normals[i];
-            Debug.LogFormat("vn {0} {1} {2}", vn.x, vn.y, vn.z);
-        }
-
-        for (int i = 0; i < mesh.triangles.Length / 3; i++)
-        {
-            int i0 = i * 3;
-            int i1 = i * 3 + 1;
-            int i2 = i * 3 + 2;
-            Debug.LogFormat("tri {0} {1} {2}", mesh.triangles[i0], mesh.triangles[i1], mesh.triangles[i2]);
-        }
+        DebugMesh(meshFilter.mesh);
     }
 
     void TestRay(int x, int y)
