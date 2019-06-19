17ae19193ea2af571fd643b967fb33cd68dcab33
ben <mpsoft_2005@126.com>
2019-06-19 23:40:45 +0800

    only diffuse shading

--- Ladybug_unity/Assets/Test_02-Plane/Scripts/CustomRotation.cs
+++ Ladybug_unity/Assets/Test_02-Plane/Scripts/CustomRotation.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class CustomRotation : MonoBehaviour
@@ -7,6 +8,7 @@ public class CustomRotation : MonoBehaviour
     public Transform lookAtTarget;
     public Light light;
     public MeshFilter meshFilter;
+    public bool capture;
 
     void PrintVector3(Vector3 v)
     {
@@ -100,6 +102,27 @@ public class CustomRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (capture)
+        {
+            capture = false;
 
+            RenderTexture oldTexture;
+            RenderTexture renderTexture = new RenderTexture(640, 480, 24);
+
+            oldTexture = Camera.main.targetTexture;
+            Camera.main.targetTexture = renderTexture;
+            Camera.main.Render();
+            Camera.main.targetTexture = oldTexture;
+
+            oldTexture = RenderTexture.active;
+            RenderTexture.active = renderTexture;
+            Texture2D texture = new Texture2D(renderTexture.width, renderTexture.height);
+            texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            texture.Apply();
+            RenderTexture.active = oldTexture;
+
+            byte[] raw = texture.EncodeToJPG();
+            File.WriteAllBytes("mytest.jpg", raw);
+        }
     }
 }
